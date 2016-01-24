d5e363be505f60f310c5ad1112614fd265808e89
Gerard <Gerard@DESKTOP-754PLNR>
2016-01-24 17:24:18 +0100

    Siguiente paso es: - implantar bien los toString de vehículo y plaza - Desarrollar las plazas como pilas

--- src/parking/Parking.java
+++ src/parking/Parking.java
@@ -14,9 +14,12 @@ public class Parking {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Coche c=new Coche("25262", "adgdsa", "Corto");
-        System.out.println(c.getTipoc());      
+        Moto c=new Moto("2653-G", "35597712Z");
         
+       // System.out.println(c.toString());      
+        Plaza p=new Plaza(35, "Primero");
+        p.setVehiculo(c);
+        System.out.println(p.toString());
     }
     
 }
--- src/parking/Plaza.java
+++ src/parking/Plaza.java
@@ -1,26 +1,28 @@
 /*atributos: vehiculo que alquila la plaza, n? de plaza, sótano donde se encuentra, tipo 'C' para coche y 'M'
-para moto.
-métodos: constructor con n? plaza y sótano
+ para moto.
+ métodos: constructor con n? plaza y sótano
  get() y set() de cada atributo.
-*/
+ */
 package parking;
 
 public class Plaza {
-    protected Vehiculo vehiculo;
-    protected Integer nplaza, sotano;
+
+    protected Vehiculo v;
+    protected String sotano;
+    protected Integer nplaza;
     protected Character tipov;
 
-    public Plaza(Integer nplaza, Integer sotano) {
+    public Plaza(Integer nplaza, String sotano) {
         this.nplaza = nplaza;
         this.sotano = sotano;
     }
 
     public Vehiculo getVehiculo() {
-        return vehiculo;
+        return v;
     }
 
     public void setVehiculo(Vehiculo vehiculo) {
-        this.vehiculo = vehiculo;
+        this.v = vehiculo;
     }
 
     public Integer getNplaza() {
@@ -31,11 +33,11 @@ public class Plaza {
         this.nplaza = nplaza;
     }
 
-    public Integer getSotano() {
+    public String getSotano() {
         return sotano;
     }
 
-    public void setSotano(Integer sotano) {
+    public void setSotano(String sotano) {
         this.sotano = sotano;
     }
 
@@ -46,25 +48,44 @@ public class Plaza {
     public void setTipov(Character tipo) {
         this.tipov = tipo;
     }
-   /*int precio() que calcula cuánto se paga según tipo de vehículo: Si es moto 25€, si es coche
-corto 40€ y si es coche largo 55€. Si el sótano es el segundo se restan 5€.
-String toString() que visualizará la siguiente información:
-N? Plaza: 58 Sótano: Segundo
-Vehículo: 6785-FLM Seat Ibiza Azul (solo se mostrará si está ocupada por un vehículo)
-Precio: 35 euros (solo se mostrará si está ocupada por un vehículo)*/
-    public Integer calculaPrecio(Vehiculo v){
-        int precio=0;
-        if (tipov.equals("M")){
-            precio = 5;
+    /*int precio() que calcula cuánto se paga según tipo de vehículo: Si es moto 25€, si es coche
+     corto 40€ y si es coche largo 55€. Si el sótano es el segundo se restan 5€.
+     String toString() que visualizará la siguiente información:
+     */
+
+    public Integer calculaPrecio(Vehiculo v) {
+        int precio = 0;
+        if (tipov.equals("M")) {
+            precio = 25;
+        } else {
+            if (v instanceof Coche) {
+                Coche c = (Coche) v;
+                if (c.getTipoc().equals("Corto")) {
+                    precio = 40;
+                } else {
+                    precio = 55;
+                }
+            }
         }
-        else{
-            if (v instanceof Coche){
-                Coche c= (Coche) v;
-               if (c.getTipoc().equals("Corto")){
-                   
-               } 
-            } 
+        if (this.sotano.equals("Segundo")) {
+            precio = precio - 5;
         }
         return precio;
+    }
+    /*N? Plaza: 58 Sótano: Segundo
+     Vehículo: 6785-FLM Seat Ibiza Azul (solo se mostrará si está ocupada por un vehículo)
+     Precio: 35 euros (solo se mostrará si está ocupada por un vehículo)*/
+
+    @Override
+    public String toString() {
+        String text;
+        text = ("Número Plaza: " + getNplaza() +" "+ "Sótano: "+ getSotano());
+        if (v!=null){
+            text.concat("\n"+v.toString());
+        }
+        else{
+            text.concat("\n+plaza vacía");
+        }
+        return text;
     }
 }
